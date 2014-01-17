ea7a38b2dbfeae047a3291f636b2e2f30a6e1865
Nicolas Palix <nicolas.palix@imag.fr>
2014-01-17 15:11:16 +0100

    Add gumtree parser

--- herodotos/diff/gumtree.ml
+++ herodotos/diff/gumtree.ml
@@ -2,54 +2,88 @@ open Xml
 
 exception MalformedXML
 
+(* begin line * begin col * end line * end col *)
+type position = int * int * int * int
+type action =
+    Insert of position
+  | Move of position * position (* before * after *)
+  | Delete of position
+
 let diffcmd = "gumtree "
 
+let get_type attrs =
+  snd (List.find (fun (key, value) -> key = "type") attrs)
+
 let get_beginline attrs = 
-  let str = snd (List.find (fun (key, value) -> key = "line") attrs) in
+  let str = snd (List.find (fun (key, value) -> key = "begin_line") attrs) in
   int_of_string str
 
 let get_begincol attrs =
-  let str = snd (List.find (fun (key, value) -> key = "line") attrs) in
+  let str = snd (List.find (fun (key, value) -> key = "begin_col") attrs) in
   int_of_string str
     
 let get_endline attrs =
-  let str = snd (List.find (fun (key, value) -> key = "line") attrs) in
+  let str = snd (List.find (fun (key, value) -> key = "end_line") attrs) in
   int_of_string str
 
 let get_endcol attrs =
-  let str = snd (List.find (fun (key, value) -> key = "line") attrs) in
+  let str = snd (List.find (fun (key, value) -> key = "end_col") attrs) in
   int_of_string str
+
+let get_pos attributes =
+  (get_beginline attributes,
+   get_begincol attributes,
+   get_endline attributes,
+   get_endcol attributes)
 
 let parse_position xml =
   match xml with
       Element("position", attributes, children) ->
-	(get_beginline attributes)
+	get_pos attributes
     | _ -> raise MalformedXML  
 
 let get_before children =
-  let Element(_, _, positions) = List.find (fun xml ->
+  let elem = List.find (fun xml ->
     match xml with
 	Element("before", _, _) -> true
       | _ -> false
   ) children in
-  match positions with
-      [position] -> parse_position position
+  match elem with
+      Element(_, _, positions) ->
+	(match positions with
+	    [position] -> parse_position position
+	  | _ -> raise MalformedXML)
     | _ -> raise MalformedXML
 
 let get_after children =
-  let Element(_, _, positions) = List.find (fun xml ->
+  let elem = List.find (fun xml ->
     match xml with
 	Element("after", _, _) -> true
       | _ -> false
   ) children in
-  match positions with
-      [position] -> parse_position position
+  match elem with
+      Element(_, _, positions) ->
+	(match positions with
+	    [position] -> parse_position position
+	  | _ -> raise MalformedXML)
     | _ -> raise MalformedXML
 
-let parse_edition xml =
+let parse_action xml =
   match xml with
-      Element("edition", attributes, children) ->
-	(get_before children, get_after children)
+      Element("action", attributes, children) ->
+	(
+	  match get_type attributes with
+	      "Insert" -> Insert (get_after children)
+	    | "Move"   -> Move (get_before children, get_after children)
+	    | "Delete" -> Delete (get_pos attributes)
+	    | _ -> raise MalformedXML
+	)
+    | _ -> raise MalformedXML
+
+let parse_actions xml =
+  match xml with
+      Element("actions", attributes, children) ->
+	List.map parse_action children
     | _ -> raise MalformedXML
 
 let parse_diff v prefix file =
@@ -57,7 +91,6 @@ let parse_diff v prefix file =
   let x = Xml.parse_file file in
   print_endline (Xml.to_string x);
   []
-
 
 let compute_new_pos_with_gumtree (diffs: Ast_diff.diffs) file ver pos : Ast_diff.lineprediction * int * int =
   Debug.profile_code_silent "Diff.compute_new_pos_with_findhunk"
