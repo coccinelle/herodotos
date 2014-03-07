1804d89237dae971c337099ffaf1b4c052ec5556
Nicolas Palix <nicolas.palix@imag.fr>
2014-03-07 11:58:17 +0100

    Add new test cases

---
+++ herodotos/demo-exp/tests/move3/ver0/test.c
@@ -0,0 +1,14 @@
+// File ver 0
+
+#include <stdio.h>
+
+int main() {
+	int err = 0, err2 = 0;
+
+	error(err);
+	printf("This is"
+	       "ver 0");
+
+	error(err2);
+	return 0;
+}
---
+++ herodotos/demo-exp/tests/move4/ver1/test.c
@@ -0,0 +1,13 @@
+// File ver 0
+
+#include <stdio.h>
+
+int main() {
+	int err = 0, err2 = 0;
+
+	printf("This is"
+	       "ver 0");
+
+	error(err); error(err2);
+	return 0;
+}
