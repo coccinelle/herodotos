8ce178bca6256764ddc581e06dcc0408432cad5b
Nicolas Palix <nicolas.palix@imag.fr>
2014-04-22 16:51:59 +0200

    Remove Xml from the core

--- herodotos/diff/gumtree.ml
+++ herodotos/diff/gumtree.ml
@@ -3,69 +3,6 @@ exception Unexpected of string
 
 let diffcmd = "gumtree --output asrc "
 
-(*
-open Xml
-exception MalformedXML
-
-(* Get positions in reference tree - With asrc option, it is the source tree *)
-(* Get positions in the associated tree - With asrc, it is the destination tree *)
-let get_pos inassociated attrs =
-  let searchedkey = (if inassociated then "other_" else "") ^ "pos" in
-  let str = snd (List.find (fun (key, value) -> key = searchedkey) attrs) in
-  int_of_string str
-
-let get_length inassociated attrs =
-  let searchedkey = (if inassociated then "other_" else "") ^ "length" in
-  let str = snd (List.find (fun (key, value) -> key = searchedkey) attrs) in
-  int_of_string str
-
-let get_beginline inassociated attrs =
-  let searchedkey = (if inassociated then "other_" else "") ^ "line_before" in
-  let str = snd (List.find (fun (key, value) -> key = searchedkey) attrs) in
-  int_of_string str
-
-let get_begincol inassociated attrs =
-  let searchedkey = (if inassociated then "other_" else "") ^ "col_before" in
-  let str = snd (List.find (fun (key, value) -> key = searchedkey) attrs) in
-  int_of_string str
-
-let get_endline inassociated attrs =
-  let searchedkey = (if inassociated then "other_" else "") ^ "line_after" in
-  let str = snd (List.find (fun (key, value) -> key = searchedkey) attrs) in
-  int_of_string str
-
-let get_endcol inassociated attrs =
-  let searchedkey = (if inassociated then "other_" else "") ^ "col_after" in
-  let str = snd (List.find (fun (key, value) -> key = searchedkey) attrs) in
-  int_of_string str
-
-(**)
-
-let dummy_pos = (0,0,0,0,0,0)
-let get_pos inassociated attributes =
-  (get_pos inassociated attributes,
-   get_length inassociated attributes,
-   get_beginline inassociated attributes,
-   get_begincol inassociated attributes,
-   get_endline inassociated attributes,
-   get_endcol inassociated attributes)
-
-let rec parse_tree xml =
-  match xml with
-      Element("tree", attributes, children) ->
-	let pos_before = get_pos false attributes in 
-	let pos_after =
-	  try
-	    Some (get_pos true attributes)
-	  with _ -> None
-	in 
-	Ast_diff.Tree(pos_before, pos_after, List.map parse_tree children)
-    | _ ->
-      print_endline "parse_tree";
-      print_endline (Xml.to_string xml);
-      raise MalformedXML
-*)
-
 let parse_diff v prefix file =
   if v then print_endline ("Parsing "^file);
   try
