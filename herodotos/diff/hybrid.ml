3d8c35250c8f4e22ee228121d88f04c2cd0b510c
Nicolas Palix <nicolas.palix@imag.fr>
2014-07-07 11:11:17 +0200

    Run gumtree only if cache file doesn't already exists

--- herodotos/diff/hybrid.ml
+++ herodotos/diff/hybrid.ml
@@ -18,17 +18,18 @@ let make_path prefix ver file =
 let alt_new_pos (diffs: Ast_diff.diffs) file ver pos : bool * (Ast_diff.lineprediction * int * int) =
   let gumfile = make_path !gumtree ver file in
   if !Misc.debug then Printf.eprintf "GNU Diff correlation failed. Trying Gumtree with %s\n" gumfile;
-  (try
-     let cmd = List.assoc gumfile !gumtree_cmd in
-     if !verbose then prerr_endline ("Looking for "^gumfile ^", will run "^cmd);
-     match
-       Unix.system cmd
-     with
-	 Unix.WEXITED 0 -> ()
-       | Unix.WEXITED 1 -> ()
-       | Unix.WEXITED i -> prerr_endline ("*** FAILURE *** Code:" ^(string_of_int i) ^" "^ cmd)
-       | _ -> prerr_endline ("*** FAILURE *** " ^cmd)
-   with Not_found -> ());
+  if not Sys.file_exists then
+    (try
+       let cmd = List.assoc gumfile !gumtree_cmd in
+       if !verbose then prerr_endline ("Looking for "^gumfile ^", will run "^cmd);
+       match
+	 Unix.system cmd
+       with
+	   Unix.WEXITED 0 -> ()
+	 | Unix.WEXITED 1 -> ()
+	 | Unix.WEXITED i -> prerr_endline ("*** FAILURE *** Code:" ^(string_of_int i) ^" "^ cmd)
+	 | _ -> prerr_endline ("*** FAILURE *** " ^cmd)
+     with Not_found -> ());
   let diffs = Gumtree.parse_diff !verbose (!gumtree^Filename.dir_sep) gumfile in
   Gumtree.compute_new_pos_with_gumtree diffs file ver pos
 
