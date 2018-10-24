6f46357cf900d9080dca17f94970ff7cbfd54fe5
Nicolas Palix <nicolas.palix@imag.fr>
2018-10-24 22:38:24 +0200

    Add fooldir script to let 'init' finds the project's directories

---
+++ herodotos/test/fooldir.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+mkdir -p ./projects/openssl/openssl-0.9.8j
+mkdir ./projects/openssl/openssl-0.9.8i
+mkdir ./projects/openssl/openssl-0.9.8h
+mkdir ./projects/openssl/openssl-0.9.8g
+mkdir ./projects/openssl/openssl-0.9.8e
+mkdir ./projects/openssl/openssl-0.9.8d
+mkdir ./projects/openssl/openssl-0.9.8c
+mkdir ./projects/openssl/openssl-0.9.8b
+mkdir  ./projects/openssl/openssl-0.9.8a
+mkdir -p ./projects/wine/wine-1.1.11
+mkdir ./projects/wine/wine-1.1.6
+mkdir ./projects/wine/wine-1.1.1
+mkdir ./projects/wine/wine-0.9.60
+mkdir ./projects/wine/wine-0.9.54
+mkdir ./projects/wine/wine-0.9.47
+mkdir ./projects/wine/wine-0.9.41
+mkdir ./projects/wine/wine-0.9.36
+mkdir ./projects/wine/wine-0.9.30
+mkdir ./projects/wine/wine-0.9.26
+mkdir ./projects/wine/wine-0.9.21
+mkdir ./projects/wine/wine-0.9.16
+mkdir ./projects/wine/wine-0.9.10
+mkdir ./projects/wine/wine-0.9.5
+mkdir ./projects/wine/wine-0.9
+mkdir ./projects/wine/wine-20050830
+mkdir -p ./projects/vlc/0.9.8a
+mkdir ./projects/vlc/0.9.4
+mkdir ./projects/vlc/0.9.0
+mkdir ./projects/vlc/0.8.6i
+mkdir ./projects/vlc/0.8.6h
+mkdir ./projects/vlc/0.8.6g
+mkdir ./projects/vlc/0.8.6f
+mkdir ./projects/vlc/0.8.6e
+mkdir ./projects/vlc/0.8.6d
+mkdir ./projects/vlc/0.8.6c
+mkdir ./projects/vlc/0.8.6b
+mkdir ./projects/vlc/0.8.6a
+mkdir ./projects/vlc/0.8.6
+mkdir ./projects/vlc/0.8.5
+mkdir ./projects/vlc/0.8.4b
+mkdir ./projects/vlc/0.8.4
+mkdir ./projects/vlc/0.8.2
+mkdir -p ./projects/linux/linux-2.6.28
+mkdir ./projects/linux/linux-2.6.27
+mkdir ./projects/linux/linux-2.6.26
+mkdir ./projects/linux/linux-2.6.25
+mkdir ./projects/linux/linux-2.6.24
+mkdir ./projects/linux/linux-2.6.23
+mkdir ./projects/linux/linux-2.6.22
+mkdir ./projects/linux/linux-2.6.21
+mkdir ./projects/linux/linux-2.6.20
+mkdir ./projects/linux/linux-2.6.19
+mkdir ./projects/linux/linux-2.6.18
+mkdir ./projects/linux/linux-2.6.17
+mkdir ./projects/linux/linux-2.6.16
+mkdir ./projects/linux/linux-2.6.15
+mkdir ./projects/linux/linux-2.6.14
+mkdir ./projects/linux/linux-2.6.13
+
